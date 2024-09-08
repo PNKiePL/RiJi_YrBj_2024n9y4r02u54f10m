37149e30f2331750e8d3b35e2ef3387ecf8d84d6
旁白 <63941028+PNKiePL@users.noreply.github.com>
2024-09-08 16:20:50 +0800

    Add files via upload

---
+++ gpd wm2/日记 2024年1月26日.cpp
@@ -0,0 +1,9 @@
+﻿日记 2024年1月26日.cpp
+/*11:14:06*/
+睡不好,能感觉到 {/*怎么那个yibosi透屏键盘五包+背部变弯+背部垫子中间上面起来了*/思考,睁眼,左侧睡,肩膀疼,挠腿,挠胳膊/*挠腿不是很正常的;感觉也不完整,有轻微的疼或者痒.醒来后才感觉膝盖疼; ;>.>, */};也就是在旁观,其实没有完整的记忆,也不怎么能控制.
+背部的胳膊以下垫一只手很爽,人的背部都撑起来,不会驼背. 右脚右手在慢慢的出汗
+
+2024年1月26日11:41:39
+黄金体验 {给自己更换身体,创造出能适应一切的身体}
+
+fjijdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkkkkkjfsidjjjkjjjjjjjjjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkkdfsjlei            jjjjjjjjdkssssssssssjjjjjjjjjjefkjijseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeejifjidskfkljeklfi          
